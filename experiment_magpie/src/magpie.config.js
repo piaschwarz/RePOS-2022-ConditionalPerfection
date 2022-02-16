22787afe89a025054279c571a456a979b8fdbb8d
piaschwarz <38730179+piaschwarz@users.noreply.github.com>
2022-02-16 10:31:20 +0100

    Add files via upload

---
+++ experiment_magpie/src/magpie.config.js
@@ -0,0 +1,7 @@
+export default {
+  mode: 'directLink',
+  experimentId: '299',
+  serverUrl: 'https://magpie-demo.herokuapp.com/',
+  serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
+  contactEmail: 'pi.schwarz@student.uni-tuebingen.de',
+};
---
+++ experiment_magpie/vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.csv$/,
+          loader: 'csv-loader',
+          options: {
+            dynamicTyping: true,
+            header: true,
+            skipEmptyLines: true
+          }
+        }
+      ]
+    }
+  },
+  lintOnSave: false,
+  pluginOptions: {
+    lintStyleOnBuild: false,
+    stylelint: {}
+  },
+  publicPath: '/'
+};
