c320904e54b479a3e445c174f3fd70a1f9374736
piaschwarz <38730179+piaschwarz@users.noreply.github.com>
2022-02-16 10:36:31 +0100

    Delete src directory

--- src/magpie.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  mode: 'directLink',
-  experimentId: '299',
-  serverUrl: 'https://magpie-demo.herokuapp.com/',
-  serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
-  contactEmail: 'pi.schwarz@student.uni-tuebingen.de',
-};
