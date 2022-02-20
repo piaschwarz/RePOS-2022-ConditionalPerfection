143d157e83e2d3c3ff5c1cd7e59a7ed93cdd54c9
Miriam1327 <56584097+Miriam1327@users.noreply.github.com>
2022-02-20 20:57:27 +0100

    updated file

--- statistical_analysis/analysis_farr.py
+++ statistical_analysis/analysis_farr.py
@@ -27,16 +27,14 @@ def exclude_participants(filename, column_names):
     :param column_names: a list with column names as extracted in get_colum_names()
     :return: a set of participant ids with information
     """
-    excludes = set()
+    excludes_long, excludes_short, line_number = set(), set(), set()
     submission_id = 0
     with open(filename) as f:
-        print("open")
         file = csv.reader(f, delimiter=',')
 
         filler_count, time_count = 0, 0
         # process every line in the file (except header)
-        for row in file:
-            print("row")
+        for num, row in enumerate(file):
             # if submission id does not match the first item in the row, the participant has changed
             if submission_id != row[0]:
                 # assign new submission id
@@ -45,16 +43,18 @@ def exclude_participants(filename, column_names):
             # we want to exclude if the submission id is the same
             else:
                 # we want to eliminate participants according to the following conditions:
-                #   1. they know either logic or pragmatics             PIA: or are not German native speakers -> exclude complete participant
-                #   2. they answered at least 3 fillers incorrectly     PIA: exclude complete participant
-                #   3. they responded too fast (< 10s)                  PIA: exclude only the item, not complete participant
-                # TODO: did I forget about something?
+                #   1. they know logic or pragmatics or are no German native speaker -> exclude complete participant
+                # TODO: What happens if there is no value for the above information?
+
+                #   2. they answered at least 3 fillers incorrectly -> exclude complete participant
+                #   3. they responded too fast (< 10s) -> exclude only the item, not the participant
 
                 # 1. check whether answer to either 'knows_logic' or 'knows_pragmatics' is 'Ja'
                 # also check whether participant is German native
                 if (row[column_names.index('knows_logic')] or row[column_names.index('knows_pragmatics')]) == 'Ja' or \
                         row[column_names.index('german_native')] == 'Nein':
-                    excludes.add(str(submission_id)+" log_prag_ger")
+                    excludes_long.add(str(submission_id)+" log_prag_ger")
+                    excludes_short.add(submission_id)
 
                 # 2. check number of correctly answered fillers
                 # count if type is 'FALSE' AND response is 'Ja' OR type is 'TRUE' AND response is 'Nein'
@@ -65,21 +65,38 @@ def exclude_participants(filename, column_names):
                     filler_count += 1
                     # if 3 is reached, add participant with note and reset filler_count
                     if filler_count == 3:
-                        excludes.add(str(submission_id)+" fillers")
+                        excludes_long.add(str(submission_id)+" fillers")
+                        excludes_short.add(submission_id)
                         filler_count = 0
 
-                # 3. check response time < 10000 (10s) - get difference as absolute number      PIA: response_time and stopwatch_ms should be added, not subtracted
-                if abs(int(row[column_names.index('response_time')]) - int(row[column_names.index('stopwatch_ms')])) < \
-                        10000:
-                    time_count += 1
-                    # if 5 is reached, add participant with note and reset time_count           PIA: I would not wait till 5 but exclude only single item if < 10s
-                    # TODO: NOTE: I set a limit of 5 for the test phase, this can be adjusted
-                    if time_count == 5:
-                        excludes.add(str(submission_id)+" too fast")
-                        time_count = 0
-        return sorted(excludes)
+                # 3. check response time < 10000 (10s) - get sum as absolute number
+                if int(row[column_names.index('response_time')]) + int(row[column_names.index('stopwatch_ms')]) < 10000:
+                    excludes_long.add(str(num)+" timing")
+                    # add ine number as negative number to not confuse it with the submission id
+                    line_number.add(num)
+        return sorted(excludes_short), sorted(line_number)
+
+
+def write_output(new_file, original_file, excludes):
+    """
+    create a new file with the participants remaining after the exclude
+    :param new_file: new csv file to write output to
+    :param original_file: original data containing all participants
+    :param excludes: the output of exclude_participants()
+    """
+    # open both files
+    original = open(original_file, 'r')
+    with open(new_file, 'w') as copied:
+        writer = csv.writer(copied, delimiter=",")
+        reader = csv.reader(original, delimiter=",")
+        # iterate over rows and get line number for record
+        for num, row in enumerate(reader):
+            # only write the content of the row if the line number is not part of the excludes
+            # and the id is not in excludes
+            if num not in excludes[1] and not any(id in excludes[0] for id in row):
+                writer.writerow(row)
 
 
 if __name__ == '__main__':
-    print(exclude_participants('results_test.csv', get_colum_names('results_test.csv')))
-
+    print(write_output('new_file.csv', 'test.csv',
+                       exclude_participants('test.csv', get_colum_names('test.csv'))))
